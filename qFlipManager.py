4c94e97f1ee40284168804fc654806905de73bfd
alexwastook <deudonalex@gmail.com>
2022-08-09 04:20:06 +0200

    initial commit

---
+++ qFlipManager.py
@@ -0,0 +1,23 @@
+from requests import get
+import wget
+import os
+import json
+
+def start(filename):
+	os.system("sudo ./"+filename)
+url = 'https://update.flipperzero.one/qFlipper/directory.json'
+request = get(url)
+data = dict(request.json())
+link = data["channels"][2]["versions"][0]["files"][2]["url"]
+filename = link.split("/")[-1]
+
+if not os.path.exists(filename):
+	print("New version available")
+	print("Deleting previous version"...)
+	os.system("rm ./*.AppImage")
+	print("Downloading...")
+	wget.download(link, filename)
+	print("Setting things up...")
+	os.system("chmod +x "+filename)
+	print("Starting...")
+start(filename)
